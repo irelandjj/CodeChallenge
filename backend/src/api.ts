b95de71204a1be0f637fa02d72762b9b26839f6b
John <irelandjj@gmail.com>
2023-11-02 13:32:58 +0100

    Added getEmployeeChildren and adjusted createEmployee

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,32 +1,38 @@
 import { Request, Response } from 'express';
 import { Employee, Developer, Manager, EmployeeType, ProgrammingLanguage, Departments } from './models';
 
-const employees: { [key: number]: Employee } = {};
-let employeeCounter = 0;
+export const createEmployee = (req: Request, res: Response, employees: { [key: number]: Employee }, employeeCounter: { count: number }) => {
+    const { name, layer, type, department, programmingLanguage, parentId } = req.body;
+    const id = ++employeeCounter.count;
+    let employee: Employee;
+    const employeeType = parseInt(type);
 
-export const createEmployee = (req: Request, res: Response) => {
-  const { name, layer, type, department, programmingLanguage, parentId } = req.body;
-  const id = ++employeeCounter;
-  let employee: Employee;
-  const employeeType = parseInt(type);
-
-  if (employeeType === EmployeeType.Manager) {
-    employee = new Manager(id, name, layer, department);
-  } else if (employeeType === EmployeeType.Developer) {
-    employee = new Developer(id, name, layer, programmingLanguage);
-  } else {
-    return res.status(400).send('Invalid employee type');
-  }
-
-  if (parentId) {
-    const parent = employees[parentId] as Manager;
-    if (!parent) {
-      return res.status(404).send('Parent employee not found');
+    if (employeeType === EmployeeType.Manager) {
+        employee = new Manager(id, name, layer, department);
+    } else if (employeeType === EmployeeType.Developer) {
+        employee = new Developer(id, name, layer, programmingLanguage);
+    } else {
+        return res.status(400).send('Invalid employee type');
     }
-    parent.addEmployee(employee);
-    employee.parent = parent;
-  }
+    const parentIdInt = parseInt(parentId);
+    if (parentIdInt) {
+        const parent = employees[parentIdInt] as Manager;
+        if (!parent) {
+            return res.status(404).send('Parent employee not found');
+        }
+        parent.addEmployee(employee);
+        employee.parent = parent;
+    }
 
-  employees[id] = employee;
-  res.status(201).json(employee);
+    employees[id] = employee;
+    res.status(201).json({ id: employee.id, message: "Employee created successfully" });
+};
+
+export const getEmployeeChildren = (req: Request, res: Response, employees: { [key: number]: Employee }) => {
+    const parent = employees[parseInt(req.params.id)] as Manager;
+    if (!parent) {
+        return res.status(404).send('Employee not found');
+    }
+    const employeeIds = parent.employees.map(emp => emp.id);
+    res.json(employeeIds);
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { createEmployee } from './api';
+import { createEmployee, getEmployeeChildren } from './api';
+import { Manager, Departments, Employee } from './models';
 
 const app = express();
 const PORT = 3000;
@@ -9,8 +10,16 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/api/employees', createEmployee);
+const employees: { [key: number]: Employee } = {};
+let employeeCounter = { count: 0 };
+
+app.post('/api/employees', (req, res) => createEmployee(req, res, employees, employeeCounter));
+app.get('/api/employees/:id/children', (req, res) => getEmployeeChildren(req, res, employees));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Initialize a default manager
+const defaultManager = new Manager(++employeeCounter.count, 'Lars Ulrich', 1, Departments.IT);
+employees[defaultManager.id] = defaultManager;
